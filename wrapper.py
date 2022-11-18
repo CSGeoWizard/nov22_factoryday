6f117ae12d55e0e60fba493dfba4acb7bc2752c2
bdw2126 <70724379+bdw2126@users.noreply.github.com>
2022-11-18 10:18:37 -0500

    Add files via upload

---
+++ wrapper.py
@@ -0,0 +1,12 @@
+import requests
+
+API_KEY = '3faf1d1d4c95245aff49d70ebaf587b9'
+lat = '40.4406'
+lon = '79.9959'
+start = '1666119016'
+end = '1668801016'
+request = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + API_KEY
+
+response = requests.get(request)
+
+print(response.json()['main']['temp_max'])
